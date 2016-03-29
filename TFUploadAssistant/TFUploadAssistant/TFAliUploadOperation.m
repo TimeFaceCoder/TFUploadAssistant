47366ea53e3a9fb40d26a26499a6e0bb13d4db73
Melvin <m.yang0204@gmail.com>
2016-03-29 09:12:50 +0800

    修改object是否存在检测方法

--- TFUploadAssistant/TFUploadAssistant/TFAliUploadOperation.m
+++ TFUploadAssistant/TFUploadAssistant/TFAliUploadOperation.m
@@ -75,58 +75,91 @@
 
 - (void)start {
     //    __weak __typeof(self)weakSelf = self;
-    //检测文件是否存在
+    
     NSTimeInterval startTime = [[NSDate date] timeIntervalSince1970];
     OSSClient *client = [[TFUploadAssistant sharedInstanceWithConfiguration:_config] client];
-    BOOL objectExist = [client doesObjectExistInBucket:_config.aliBucket objectKey:_key error:nil];
-    if (objectExist) {
-        _progressHandler(_key,_token,1);
-        _completionHandler(nil,_key,_token,YES);
-        TFULogDebug(@"object :%@ exist",_key);
-        return;
-    }
-    OSSPutObjectRequest *put = [OSSPutObjectRequest new];
-    put.contentType = [OSSUtil detemineMimeTypeForFilePath:nil uploadName:_key];
-    put.bucketName = _config.aliBucket;
-    put.objectKey = _key;
-    put.uploadingData = _data;
-    put.contentMd5 = [OSSUtil base64Md5ForData:put.uploadingData];
-    put.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
-        float progress = (float)totalByteSent/(float)totalBytesExpectedToSend;
-        //        __typeof(&*weakSelf) strongSelf = weakSelf;
-        //        if (strongSelf && strongSelf.progressHandler) {
-        //            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-        //            }];
-        //            strongSelf.progressHandler(strongSelf.key,strongSelf.token,progress);
-        //            
-        //        }
-        _progressHandler(_key,_token,progress);
-    };
-    OSSTask *putTask = [client putObject:put];
-    [putTask continueWithBlock:^id(OSSTask *task) {
-        //        __typeof(&*weakSelf) strongSelf = weakSelf;
-        TFResponseInfo *info = nil;
-        NSTimeInterval endTime = [[NSDate date] timeIntervalSince1970];
-        if (task.error) {
-            TFULogDebug(@"%@", task.error);
-            info = [TFResponseInfo responseInfoWithNetError:task.error duration:endTime - startTime];
+    //检测文件是否存在
+    [self objectExist:_key completionBlock:^(BOOL result) {
+        if (result) {
+            _progressHandler(_key,_token,1);
+            _completionHandler(nil,_key,_token,YES);
+            TFULogDebug(@"object :%@ exist",_key);
+            return;
         }
-        OSSPutObjectResult * result = task.result;
-        info = [[TFResponseInfo alloc] initWithStatusCode:result.httpResponseCode withDuration:endTime - startTime withBody:nil];
-        if (result.httpResponseCode == 200) {
-            //上传成功
-            //            if (strongSelf && strongSelf.completionHandler) {
-            //                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            //                }];
-            //                strongSelf.completionHandler(info,strongSelf.key,strongSelf.token,nil);
-            //            }
-            if (_completionHandler) {
-                _completionHandler(info,_key,_token,YES);
-            }
+        else {
+            OSSPutObjectRequest *put = [OSSPutObjectRequest new];
+            put.contentType = [OSSUtil detemineMimeTypeForFilePath:nil uploadName:_key];
+            put.bucketName = _config.aliBucket;
+            put.objectKey = _key;
+            put.uploadingData = _data;
+            put.contentMd5 = [OSSUtil base64Md5ForData:put.uploadingData];
+            put.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
+                float progress = (float)totalByteSent/(float)totalBytesExpectedToSend;
+                //        __typeof(&*weakSelf) strongSelf = weakSelf;
+                //        if (strongSelf && strongSelf.progressHandler) {
+                //            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                //            }];
+                //            strongSelf.progressHandler(strongSelf.key,strongSelf.token,progress);
+                //
+                //        }
+                _progressHandler(_key,_token,progress);
+            };
+            OSSTask *putTask = [client putObject:put];
+            [putTask continueWithBlock:^id(OSSTask *task) {
+                //        __typeof(&*weakSelf) strongSelf = weakSelf;
+                TFResponseInfo *info = nil;
+                NSTimeInterval endTime = [[NSDate date] timeIntervalSince1970];
+                if (task.error) {
+                    TFULogDebug(@"%@", task.error);
+                    info = [TFResponseInfo responseInfoWithNetError:task.error duration:endTime - startTime];
+                }
+                OSSPutObjectResult * result = task.result;
+                info = [[TFResponseInfo alloc] initWithStatusCode:result.httpResponseCode withDuration:endTime - startTime withBody:nil];
+                if (result.httpResponseCode == 200) {
+                    //上传成功
+                    //            if (strongSelf && strongSelf.completionHandler) {
+                    //                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                    //                }];
+                    //                strongSelf.completionHandler(info,strongSelf.key,strongSelf.token,nil);
+                    //            }
+                    if (_completionHandler) {
+                        _completionHandler(info,_key,_token,YES);
+                    }
+                }
+                TFULogDebug(@"Result - requestId: %@ ",result.requestId);
+                return nil;
+            }];
         }
-        TFULogDebug(@"Result - requestId: %@ ",result.requestId);
-        return nil;
     }];
+    
+    
 }
+
+- (BOOL)objectExist:(NSString *)objectKey
+    completionBlock:(void (^)(BOOL result))completionBlock {
+    NSString *url = [NSString stringWithFormat:@"http://%@.%@/%@",_config.aliBucket,_config.aliBucketHostId,objectKey];
+    __block BOOL result = NO;
+    NSMutableURLRequest * request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    NSURLSessionConfiguration  *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession * session = [NSURLSession sessionWithConfiguration:configuration];
+    [request setHTTPMethod:@"HEAD"];
+    NSURLSessionTask * sessionTask = [session dataTaskWithRequest:request
+                                                completionHandler:^(NSData *data,
+                                                                    NSURLResponse *response,
+                                                                    NSError *error)
+                                      {
+                                          NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+                                          NSInteger code = [httpResponse statusCode];
+                                          if (code != 200) {
+                                              completionBlock(NO);
+                                          }
+                                          else {
+                                              completionBlock(YES);
+                                          }
+                                      }];
+    [sessionTask resume];
+    return result;
+}
+
 
 @end
